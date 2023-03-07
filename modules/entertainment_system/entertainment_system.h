4c43ae6ae50102a4ad6395f07b069bb0071d1a33
21amichalek <124909651+21amichalek@users.noreply.github.com>
2023-03-07 12:31:39 -0500

    Update entertainment_system.h
    finalized

--- modules/entertainment_system/entertainment_system.h
+++ modules/entertainment_system/entertainment_system.h
@@ -1,1 +1,19 @@
+//=====[#include guards - begin]===============================================
 
+#ifndef _ENTERTAINMENT_SYSTEM_H_
+#define _ENTERTAINMENT_SYSTEM_H_
+
+//=====[Declaration of public defines]=========================================
+
+#define SYSTEM_TIME_INCREMENT_MS   10
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void entertainmentSystemInit();
+void entertainmentSystemUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif // _ENTERTAINMENT_SYSTEM_H_
