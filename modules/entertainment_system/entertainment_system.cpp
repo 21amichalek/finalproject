7dce638801d5066a7778f2dbc2ce72638e1480c9
21amichalek <124909651+21amichalek@users.noreply.github.com>
2023-03-07 12:34:14 -0500

    Update entertainment_system.cpp
    finalized

--- modules/entertainment_system/entertainment_system.cpp
+++ modules/entertainment_system/entertainment_system.cpp
@@ -1,1 +1,39 @@
+//=====[Libraries]=============================================================
 
+#include "arm_book_lib.h"
+
+#include "entertainment_system.h"
+
+#include "user_interface.h"
+#include "party_features.h"
+
+//=====[Declaration of private defines]========================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
+
+void entertainmentSystemInit()
+{
+    userInterfaceInit();
+    partySystemInit();
+}
+
+void smartHomeSystemUpdate()
+{
+    userInterfaceUpdate();
+    partySystemUpdate();
+    delay(SYSTEM_TIME_INCREMENT_MS);
+}
+
+//=====[Implementations of private functions]==================================
