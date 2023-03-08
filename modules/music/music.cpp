8f3fcdbafaf4553bf695b0ec89e7d01f6703061c
ashleypontillo <124712651+ashleypontillo@users.noreply.github.com>
2023-03-08 17:17:47 -0500

    Update music.cpp

--- modules/music/music.cpp
+++ modules/music/music.cpp
@@ -2,15 +2,16 @@
 
 #include "mbed.h"
 #include "arm_book_lib.h"
+#include "music.h"
 
 //=====[Declaration of private defines]========================================
 
-#define MUSIC_DUTY_CYCLE 0.5
+#define MUSIC_DUTY_CYCLE    0.5
 
 //=====[Declaration of private data types]=====================================
 
 //bc buzzer and sending a pwm
-PwmOut musicPin(PE_11);
+PwmOut musicPin(PE_14);
 
 //=====[Declaration and initialization of public global objects]===============
 
@@ -19,22 +20,19 @@ PwmOut musicPin(PE_11);
 //=====[Declaration and initialization of private global variables]============
 
 //=====[Declarations (prototypes) of private functions]========================
-static void setPeriod(float period);
+static void setMusicPeriod(float period);
 
 //=====[Implementations of public functions]===================================
 
-
 //set period and duty cycle
 void musicInit(){
-    setPeriod(0.1f);
-    setMusicDutyCycle();
+    setMusicPeriod(0.001f);                                       
+    setMusicDutyCycle(0.5f);
 }
 
-
 //changes the volume 
-void setMusicDutyCycle()
-{
-    musicPin.write(MUSIC_DUTY_CYCLE);
+void setMusicDutyCycle(float dutyCycle){
+    musicPin.write(dutyCycle);
 }
 
 
@@ -44,5 +42,47 @@ static void setMusicPeriod(float period){
     musicPin.period(period);
 }
 
+//=====[Libraries]=============================================================
 
-//
+#include "mbed.h"
+#include "arm_book_lib.h"
+#include "music.h"
+
+//=====[Declaration of private defines]========================================
+
+#define MUSIC_DUTY_CYCLE    0.5
+
+//=====[Declaration of private data types]=====================================
+
+//bc buzzer and sending a pwm
+PwmOut musicPin(PE_14);
+
+//=====[Declaration and initialization of public global objects]===============
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of private global variables]============
+
+//=====[Declarations (prototypes) of private functions]========================
+static void setMusicPeriod(float period);
+
+//=====[Implementations of public functions]===================================
+
+//set period and duty cycle
+void musicInit(){
+    setMusicPeriod(0.001f);                                       
+    setMusicDutyCycle(0.5f);
+}
+
+//changes the volume 
+void setMusicDutyCycle(float dutyCycle){
+    musicPin.write(dutyCycle);
+}
+
+
+//vary frequency but constant duty cycle
+//changes the note 
+static void setMusicPeriod(float period){
+    musicPin.period(period);
+}
+
