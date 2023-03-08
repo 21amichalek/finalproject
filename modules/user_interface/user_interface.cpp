ef617ff2f0b72136d30ca3025d9c5b57451f227b
austingreg <124914390+austingreg@users.noreply.github.com>
2023-03-08 17:47:37 -0500

    Update user_interface.cpp

--- modules/user_interface/user_interface.cpp
+++ modules/user_interface/user_interface.cpp
@@ -1,1 +1,108 @@
+//=====[Libraries]=============================================================
 
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+#include "user_interface.h"
+
+
+#include "smart_home_system.h"
+
+#include "matrix_keypad.h"
+
+
+
+//=====[Declaration of private defines]========================================
+
+
+
+//=====[Declaration of private data types]=====================================
+
+
+
+//=====[Declaration and initialization of public global objects]===============
+
+
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+
+
+//=====[Declaration and initialization of private global variables]============
+char color[] = {'8','8','8'};
+
+
+//=====[Declarations (prototypes) of private functions]========================
+
+static char userInterfaceMatrixKeypadUpdate();
+
+
+
+
+
+
+//=====[Implementations of public functions]===================================
+
+void userInterfaceInit()
+{
+    matrixKeypadInit( SYSTEM_TIME_INCREMENT_MS );
+    
+    
+}
+
+void userInterfaceUpdate()
+{
+    userInterfaceMatrixKeypadUpdate();
+    mode1Keypad();
+    modePartyKeypad();
+    
+    
+}
+char mode1Keypad(){
+    return userInterfaceMatrixKeypadUpdate();
+}
+ void modePartyKeypad(){
+    int i = 0;
+    for (i = 0; i < 3; i++) {
+        color[i] = userInterfaceMatrixKeypadUpdate();
+    }
+
+}
+
+char readColor1(){
+    return color[0];
+}
+
+char readColor2(){
+    return color[1];
+}
+
+char readColor3(){
+    return color[2];
+}
+
+
+
+
+//=====[Implementations of private functions]==================================
+
+static char userInterfaceMatrixKeypadUpdate()
+{
+    
+    char keyReleased = matrixKeypadUpdate();
+    
+    if( keyReleased == '1'||'2'||'3'||'4'||'5'||'6'||'7') {
+        return keyReleased;
+        //this part is causing me trouble I can't quite understand how to fix it
+                
+    }
+    
+    
+    
+    
+    
+}
+
+ 
