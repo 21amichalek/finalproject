546b2f3ec350a915c09729564de55af29bc80c27
ashleypontillo <124712651+ashleypontillo@users.noreply.github.com>
2023-03-08 15:46:24 -0500

    Update music.h

--- modules/music/music.h
+++ modules/music/music.h
@@ -1,1 +1,11 @@
+//=====[#include guards - begin]===============================================
 
+#ifndef _MUSIC_H_
+#define _MUSIC_H_
+
+//=====[Libraries]=============================================================
+
+void musicInit();
+void setMusicDutyCycle();
+
+#endif // _MUSIC_H_
