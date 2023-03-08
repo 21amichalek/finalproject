60f716e2a1f607d47b6a0631da0b9437c8dc139d
austingreg <124914390+austingreg@users.noreply.github.com>
2023-03-08 17:48:11 -0500

    Update user_interface.h

--- modules/user_interface/user_interface.h
+++ modules/user_interface/user_interface.h
@@ -1,1 +1,23 @@
+//=====[#include guards - begin]===============================================
 
+#ifndef _USER_INTERFACE_H_
+#define _USER_INTERFACE_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void userInterfaceInit();
+void userInterfaceUpdate();
+
+char mode1Keypad();
+void modePartyKeypad();
+char readColor1();
+char readColor2();
+char readColor3();
+
+//=====[#include guards - end]=================================================
+
+#endif // _USER_INTERFACE_H_
