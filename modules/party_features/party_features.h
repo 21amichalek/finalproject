64bea5ae7dfca19b5552762ecd397403650ee3c7
austingreg <124914390+austingreg@users.noreply.github.com>
2023-03-08 17:50:07 -0500

    Update party_features.h

--- modules/party_features/party_features.h
+++ modules/party_features/party_features.h
@@ -1,1 +1,19 @@
+//=====[#include guards - begin]===============================================
 
+#ifndef _PARTY_FEATURES_H_
+#define _PARTY_FEATURES_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+
+
+//=====[Declarations (prototypes) of public functions]=========================
+void PartyInit();
+void PartyUpdate();
+
+
+//=====[#include guards - end]=================================================
+
+#endif // _PARTY_FEATURES_H_
