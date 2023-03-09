eaa8f4f837ed14cc8be89ae04e3e24719ed409b6
21amichalek <124909651+21amichalek@users.noreply.github.com>
2023-03-09 14:33:24 -0500

    Update party_features.cpp
    implemented temp party stop

--- modules/party_features/party_features.cpp
+++ modules/party_features/party_features.cpp
@@ -2,15 +2,21 @@
 
 #include "arm_book_lib.h"
 #include "mbed.h"
+
 #include "lighting.h"
-#include "smart_home_system.h"
 #include "matrix_keypad.h"
 #include "party_features.h"
 #include "pc_serial_com.h"
 #include "user_interface.h"
+
+#include "ldr_sensor.h"
+
 //=====[Declaration of private defines]========================================
 
+#define MAX_VOLTAGE_PARTY_STOP 0.6
+
 //=====[Declaration of private data types]=====================================
+
 typedef enum {
     PARTY_OFF,
     PARTY_MODE_SCANNING,
@@ -18,8 +24,8 @@ typedef enum {
     PARTY_MODE_2,
     PARTY_MODE_3
 } PartyState_t;
-//=====[Declaration and initialization of public global objects]===============
 
+//=====[Declaration and initialization of public global objects]===============
 
 
 //=====[Declaration of external public global variables]=======================
@@ -27,22 +33,24 @@ typedef enum {
 //=====[Declaration and initialization of public global variables]=============
 
 //=====[Declaration and initialization of private global variables]============
+
 static PartyState_t PartyState;
+
+static bool partyStopState = OFF;
+
 //=====[Declarations (prototypes) of private functions]========================
 
 //=====[Implementations of public functions]===================================
-void PartyInit(){
+
+void PartyInit()
+{
     brightControlInit();
 
     PartyState = PARTY_MODE_SCANNING;
-
-    
-
-
-
 }
 
-void PartyUpdate(){
+void PartyUpdate()
+{
     switch(PartyState) {
 
     case PARTY_OFF:
@@ -51,51 +59,57 @@ void PartyUpdate(){
 
     case PARTY_MODE_SCANNING:
         
-        if (readPartyMode() == '1') {
-            PartyState = PARTY_MODE_1;
-            
+        if ( partyStop() ) {
+            PartyState = PARTY_MODE_SCANNING;
         }
 
-        if (readPartyMode() == '2') {
+        else if (readPartyMode() == '1') {
+            PartyState = PARTY_MODE_1;
+        }
+
+        else if (readPartyMode() == '2') {
             PartyState = PARTY_MODE_2;
         }
 
-        if (readPartyMode() == '2') {
+        else if (readPartyMode() == '3') {
             PartyState = PARTY_MODE_3;
         }
-        
         break;
 
     case PARTY_MODE_1:
+        if ( partyStop() ) {
+            PartyState = PARTY_MODE_SCANNING;
+        }
         mode1Color(mode1Keypad());
-        
-
-        
         break;
 
     case PARTY_MODE_2:
+        if ( partyStop() ) {
+            PartyState = PARTY_MODE_SCANNING;
+        }
          mode2Color(readColor1(), readColor2(), readColor3());
-        
         break;
     
     case PARTY_MODE_3:
+        if ( partyStop() ) {
+            PartyState = PARTY_MODE_SCANNING;
+        }
          mode3Color(readColor1(), readColor2(), readColor3());
+        break;
 
     default:
-        
         break;
     }
-    
 }
-
-
-
-
-
-
 
 //=====[Implementations of private functions]==================================
 
-
-
-
+static bool partyStop()
+{
+    if ( ldrSensorRead() < MAX_VOLTAGE_PARTY_STOP ) {
+        partyStopState = ON;
+    } else {
+        partyStopState = OFF;
+    }
+    return partyStopState;
+}
