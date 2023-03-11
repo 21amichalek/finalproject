cc21fddda88217963d0a5418f37ec5a023ab1e0d
21amichalek <124909651+21amichalek@users.noreply.github.com>
2023-03-11 11:54:21 -0500

    Update party_features.cpp

--- modules/party_features/party_features.cpp
+++ modules/party_features/party_features.cpp
@@ -2,17 +2,22 @@
 
 #include "arm_book_lib.h"
 #include "mbed.h"
-#include "lighting.h"
+
 #include "party_features.h"
+
+#include "lighting.h"
 #include "music.h"
 #include "matrix_keypad.h"
-#include "party_features.h"
 #include "pc_serial_com.h"
 #include "user_interface.h"
 #include "ldr_sensor.h"
+
 //=====[Declaration of private defines]========================================
+
 #define MAX_VOLTAGE_PARTY_STOP 0.1
+
 //=====[Declaration of private data types]=====================================
+
 typedef enum {
     PARTY_OFF,
     PARTY_MODE_SCANNING,
@@ -20,71 +25,59 @@ typedef enum {
     PARTY_MODE_2,
     PARTY_MODE_3
 } PartyState_t;
+
 //=====[Declaration and initialization of public global objects]===============
-
-
 
 //=====[Declaration of external public global variables]=======================
 
 //=====[Declaration and initialization of public global variables]=============
 
 //=====[Declaration and initialization of private global variables]============
+
 static PartyState_t PartyState;
-
-
 static bool partyStopState = OFF;
 
-
 //=====[Declarations (prototypes) of private functions]========================
-static bool partyStop();
-//=====[Implementations of public functions]===================================
-void PartyInit(){
-    brightControlInit();
 
+static bool partyStop();
+
+//=====[Implementations of public functions]===================================
+
+void PartyInit(){
+    brightControlInit()
     PartyState = PARTY_MODE_SCANNING;
 }
 
 void PartyUpdate(){
     switch(PartyState) {
-
     case PARTY_OFF:
         PartyInit();
         break;
 
     case PARTY_MODE_SCANNING:
-        
         if (readPartyMode() == '1') {
             PartyState = PARTY_MODE_1;
-            
         }
-
         if (readPartyMode() == '2') {
             PartyState = PARTY_MODE_2;
         }
-
         if (readPartyMode() == '3') {
             PartyState = PARTY_MODE_3;
         }
-        
         break;
 
     case PARTY_MODE_1:
-        
         mode1Color(readMode1Color());
         if ( partyStop() ) {
             PartyState = PARTY_OFF;
         }
-        
         break;
 
     case PARTY_MODE_2:
-        
         mode2Color('1', '3','6');
         if ( partyStop() ) {
             PartyState = PARTY_OFF;
         }
-        
-        
         break;
     
     case PARTY_MODE_3:
@@ -95,12 +88,10 @@ void PartyUpdate(){
         setMusic(readSong()); 
         break;
     }
-    
 }
 
-
-
 //=====[Implementations of private functions]==================================
+
 static bool partyStop()
 {
     if ( ldrSensorRead() < MAX_VOLTAGE_PARTY_STOP ) {
@@ -110,6 +101,3 @@ static bool partyStop()
     }
     return partyStopState;
 }
-
-
-
