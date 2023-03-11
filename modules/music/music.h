473367b5134d8b1447eb72e25b50e3e033495f6f
21amichalek <124909651+21amichalek@users.noreply.github.com>
2023-03-11 11:45:22 -0500

    Update music.h

--- modules/music/music.h
+++ modules/music/music.h
@@ -3,15 +3,23 @@
 #ifndef _MUSIC_H_
 #define _MUSIC_H_
 
-//=====[Libraries]=============================================================
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
 
 void musicInit();
 void songPlayingWrite( bool state );
 void setMusicDutyCycle(float dutyCycle);
+
 void playSong1();
 void playSong2();
 void playSong3();
 void playSong4();
+
 void setMusic(char song);
+
+//=====[#include guards - end]=================================================
 
 #endif // _MUSIC_H_
